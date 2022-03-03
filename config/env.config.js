f7605e2d51748bc1fc03d25c1b6c9bad34a43b24
Nishikantaray <nishikantaray1@gmail.com>
2022-03-03 23:20:11 +0530

    added

--- config/env.config.js
+++ config/env.config.js
@@ -2,6 +2,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// Add a  env variable to your .env file
+
+// add below details their respective env variables
+// export EMAIL_PASSWORD=your_password
+// export EMAIL_USER=your_email
+
+
+
 module.exports = {
     port: process.env.PORT,
     email : process.env.EMAIL,
