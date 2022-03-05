690428d6f2b314901b4364b1f4989b26df99c697
Nishikantaray <nishikantaray1@gmail.com>
2022-03-05 22:33:13 +0530

    added env and routes file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { port } = require("./config/env.config");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+// const port=3000;
+app.use("/api", require("./routes/routes"));
+
+app.use("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the API"
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is listening on port ${port}`);
+});
